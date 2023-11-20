364a7b5d4a4d8961e71f61509ea6e5581b9a0173
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-11-20 21:59:43 +0300

    fixed navigation

--- Courses/urls.py
+++ Courses/urls.py
@@ -4,8 +4,14 @@ from . import views
 app_name = 'linc'
 urlpatterns = [
     path('', views.home, name='home'),
-    path('course-details', views.course_details, name='courses_details'),
+    path('course-details', views.course_details, name='course_details'),
     path('gallery', views.gallery, name='gallery'),
     path('events', views.events, name='events'),
     path('about', views.about, name='about'),
+    path('courses', views.courses, name='courses'),
+    path('contact', views.contact, name='contact'),
+    path('elements', views.elements, name='elements'),
+    path('event-details', views.event_details, name='event_details'),
+    path('blog-home', views.blog_home, name='blog_home'),
+    path('blog-single', views.blog_single, name='blog_single'),
 ]
--- Courses/views.py
+++ Courses/views.py
@@ -18,3 +18,28 @@ def about(request):
 
 def events(request):
     return render(request, 'events.html')
+
+
+def courses(request):
+    return render(request, 'courses.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def elements(request):
+    return render(request, 'elements.html')
+
+
+def event_details(request):
+    return render(request, 'event-details.html')
+
+
+def blog_home(request):
+    return render(request, 'blog-home.html')
+
+
+
+def blog_single(request):
+    return render(request, 'blog-single.html')
