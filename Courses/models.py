2fb7ee11dc1fe06030d17f8e2de392e68604e4ec
Collins331 <ochiengcollinsoduor2@gmail.com>
2023-11-27 16:08:42 +0300

    update school

--- Courses/models.py
+++ Courses/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(max_length=100, blank=False)
+    subject = models.CharField(max_length=200, blank=False)
+    message = models.TextField(blank=False)
--- Courses/views.py
+++ Courses/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.contrib import messages
+from .models import Contact
 
 # Create your views here.
 def home(request):
@@ -25,6 +28,15 @@ def courses(request):
 
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        msg = Contact(name=name, email=email, subject=subject, message=message)
+        msg.save()
+        messages.success(request, 'Your message has been sent successfully')
+        return redirect('/')
     return render(request, 'contact.html')
 
 
